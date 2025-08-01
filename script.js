e20a044c6ea8e2521371aa4313be38b5c6771e82
haoyu163 <wang_haoyu_8@students.edu.sg>
2025-08-01 22:52:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,11 @@
 const min = 0
 const max = 10
 const RNG = document.getElementById("Gambling");
-const countLabel = document.getElementById("countLabel");
-let randomNum;
+const Label1 = document.getElementById("Label1");
+const Label2 = document.getElementById("Label2");
+const Label3 = document.getElementById("Label3");
 RNG.onclick = function(){
-    randomNum = Math.floor(Math.random() * max) + min;
-    countLabel.textContent = randomNum
+    Label1.textContent = Math.floor(Math.random() * max) + min;
+    Label2.textContent = Math.floor(Math.random() * max) + min;
+    Label3.textContent = Math.floor(Math.random() * max) + min;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
