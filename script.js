021bbef007932c1357418c07cf04944e2efc75d4
haoyu163 <wang_haoyu_8@students.edu.sg>
2025-08-01 23:26:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-const min = 0
-const max = 10
+const min = 0;
+const max = 10;
 const RNG = document.getElementById("Gambling");
 const Label1 = document.getElementById("Label1");
 const Label2 = document.getElementById("Label2");
 const Label3 = document.getElementById("Label3");
-RNG.onclick = function(){
-    Label1.textContent = Math.floor(Math.random() * max) + min;
-    Label2.textContent = Math.floor(Math.random() * max) + min;
-    Label3.textContent = Math.floor(Math.random() * max) + min;
+
+RNG.onclick = function() {
+    const num1 = Math.floor(Math.random() * max) + min;
+    const num2 = Math.floor(Math.random() * max) + min;
+    const num3 = Math.floor(Math.random() * max) + min;
+    Label1.textContent = num1;
+    Label2.textContent = num2;
+    Label3.textContent = num3;
+
+    let message = "";
+    if (num1 === 7 && num2 === 7 && num3 === 7) {
+        message = "Jackpot!";
+    } else if (num1 === num2 && num2 === num3) {
+        message = "Big win!";
+    } else if (num1 === num2 || num2 === num3 || num1 === num3) {
+        message = "You win!";
+    } else {
+        message = "You lose! Try again!";
+    }
+    setTimeout(() => {
+        alert(message);
+    }, 500);
 }
